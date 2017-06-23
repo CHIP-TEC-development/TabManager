c58aca95c9d9abf02df230d60781e8192400100c
RookieTEC9 <jeremiur.ps101q@gmail.com>
2017-06-23 19:05:17 -0400

    meh

--- src/main/java/org/CHIPTEC/tabman/TabMan.java
+++ src/main/java/org/CHIPTEC/tabman/TabMan.java
@@ -13,7 +13,7 @@ public class TabMan extends JavaPlugin {
 	public static TabMan getInstance() {
 		return instance;
 	}
-	
+
 	public void onEnable() {
 		instance = this;
 		if (!this.getDataFolder().exists())
@@ -28,16 +28,17 @@ public class TabMan extends JavaPlugin {
 	/**
 	 * @author RookieTEC9
 	 * 
-	 * Gets the Files in "\\Commands" and does a Tab Executor for every one.
+	 *         Gets the Files in "\\Commands" and does a Tab Executor for every
+	 *         one.
 	 * 
-	 * Has not been tested as of 06/22/17
+	 *         Has not been tested as of 06/22/17
 	 */
 	public void registerTabbers() {
 		for (int i = 0; i < filesList.length; i++) {
-			JavaPlugin plug = (JavaPlugin) Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", "")).getPlugin();
-			Command cmd = Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", ""));			
+			JavaPlugin plug = (JavaPlugin) Bukkit.getPluginManager().getPlugin(Bukkit.getServer()
+					.getPluginCommand(String.valueOf(filesList[i]).replace(".yml", "")).getPlugin().getName());
+			Command cmd = Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", ""));
 			plug.getCommand(cmd.getName());
-			
 		}
 	}
 
