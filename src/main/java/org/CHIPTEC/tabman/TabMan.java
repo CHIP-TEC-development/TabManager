c361cb49099e953a77b4bb0374b5cce0db4c097f
RookieTEC9 <jeremiur.ps101q@gmail.com>
2017-06-22 21:15:35 -0400

    Changed registerTabbers to continue its build

--- src/main/java/org/CHIPTEC/tabman/TabMan.java
+++ src/main/java/org/CHIPTEC/tabman/TabMan.java
@@ -3,6 +3,7 @@ package org.CHIPTEC.tabman;
 import java.io.File;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TabMan extends JavaPlugin {
@@ -12,7 +13,7 @@ public class TabMan extends JavaPlugin {
 	public static TabMan getInstance() {
 		return instance;
 	}
-
+	
 	public void onEnable() {
 		instance = this;
 		if (!this.getDataFolder().exists())
@@ -28,10 +29,15 @@ public class TabMan extends JavaPlugin {
 	 * @author RookieTEC9
 	 * 
 	 * Gets the Files in "\\Commands" and does a Tab Executor for every one.
+	 * 
+	 * Has not been tested as of 06/22/17
 	 */
 	public void registerTabbers() {
 		for (int i = 0; i < filesList.length; i++) {
-			Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", "")).getTabCompleter();
+			JavaPlugin plug = (JavaPlugin) Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", "")).getPlugin();
+			Command cmd = Bukkit.getServer().getPluginCommand(String.valueOf(filesList[i]).replace(".yml", ""));			
+			plug.getCommand(cmd.getName());
+			
 		}
 	}
 
